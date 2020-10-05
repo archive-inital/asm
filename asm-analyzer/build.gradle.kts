96cc4cb0a8fbfa8cbe48f0aca1870a5637a00746
Kyle Escobar <kslrtips@gmail.com>
2020-10-05 09:59:54 -0400

    Add publishing for asm-analyzer

--- asm-analyzer/build.gradle.kts
+++ asm-analyzer/build.gradle.kts
@@ -3,3 +3,14 @@ description = "ASM Analyzer"
 dependencies {
     api(project(":asm-core"))
 }
+
+publishing {
+    publications {
+        create<MavenPublication>("asm-analyzer") {
+            groupId = "org.spectral"
+            artifactId = "asm-analyzer"
+            version = Project.version
+            from(components["java"])
+        }
+    }
+}
